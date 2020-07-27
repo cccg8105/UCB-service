5e0ef5fc41543ab9bae17828478b94f6e7c87107
César Gastón Cárdenas Córdova <cesar.car5290@gmail.com>
2020-07-27 12:28:44 -0500

    Se implementa enviroment, nodemon y babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,19 +1,19 @@
 const presets = [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: true,
-        },
+  [
+    '@babel/preset-env',
+    {
+      targets: {
+        node: true,
       },
-    ],
-    "minify",
-  ]
-  
-  const plugins = [
-    "@babel/plugin-proposal-export-default-from",
-    "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-optional-chaining",
-  ]
-  
-  module.exports = { presets, plugins }
+    },
+  ],
+  'minify',
+]
+
+const plugins = [
+  "@babel/plugin-proposal-export-default-from",
+  "@babel/plugin-proposal-export-namespace-from",
+  "@babel/plugin-proposal-optional-chaining",
+]
+
+module.exports = { presets, plugins }
